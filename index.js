2e3b4bb1df1724c190e3e07dac1b950339cd4307
julie <perianmodely.julie@gmail.com>
2022-02-28 15:58:57 +0400

    add tout les fichiers

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 
 
-fetch(index.json)
-.then(res=>console.log(res));
+// fetch("https://restcountries.com/v2/regionalbloc/eu")
+//  .then(resp=>resp.json())
+//  .then(region =>console.log(region))
+ 
+async function render(){
+    let rep = await fetch('https://restcountries.com/v3.1/region/europe');
+    let data= await rep.json();
+     console.log(data)
+
+    
+    let result="";
+
+    for(pays in data){
+       result+= `<li> ${data[pays].name.official} </li>` 
+    }
+    document.getElementById("list").innerHTML=result;
+}
+render();
+    
