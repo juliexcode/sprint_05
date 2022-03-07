f28dfff5fc8761d7662b5d4235a7b22e3987f855
julie <perianmodely.julie@gmail.com>
2022-03-07 14:48:26 +0400

    add final

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
+async function render() {
+  try {
+    let rep = await fetch("https://restcountries.com/v3.1/region/europe");
+    let data = await rep.json();
 
+    console.log(data);
 
-// fetch("https://restcountries.com/v2/regionalbloc/eu")
-//  .then(resp=>resp.json())
-//  .then(region =>console.log(region))
- 
-async function render(){
-    let rep = await fetch('https://restcountries.com/v3.1/region/europe');
-    let data= await rep.json();
-   
-      console.log(data)
+    let result = "";
 
-    
-    let result="";
-
-    for(pays in data){
-       result+= `<li> ${data[pays].name.official} </li>` 
+    for (pays in data) {
+      result += `<li> ${data[pays].name.official} </li>`;
     }
-    document.getElementById("list").innerHTML=result;
+    document.getElementById("list").innerHTML = result;
+  } catch (e) {
+    console.log(e);
+  }
 }
 render();
-    
--- table.js
+++ table.js
@@ -1,20 +1,23 @@
-async function render(){
-    let rep = await fetch('https://restcountries.com/v3.1/region/europe');
-    let data= await rep.json();
-   
-      console.log(data)
+async function render() {
+  try {
+    let response = await fetch("https://restcountries.com/v3.1/region/europe");
 
-    
-    let result="";
+    let data = await response.json();
+    console.log(data);
 
-    for(pays in data){
-        let air= data[pays].area.toLocaleString('en-US');
-        let pop=data[pays].population.toLocaleString('en-US')
-       result+= `<tr><td class="h5"> ${data[pays].name.official}</td>
-       <td> ${air}</td> 
-       <td> ${pop}</td>
-       <td> ${data[pays].capital}</td></tr>` 
+    let result = "";
+
+    for (pays in data) {
+      let air = data[pays].area.toLocaleString("en-US");
+      let pop = data[pays].population.toLocaleString("en-US");
+      result += `<tr><td class="fw-bold"> ${data[pays].name.official}</td>
+       <td class="text-end"> ${air}</td> 
+       <td class="text-end"> ${pop}</td>
+       <td> ${data[pays].capital}</td></tr>`;
     }
-    document.getElementById("tableau").innerHTML=result;
+    document.getElementById("tableau").innerHTML = result;
+  } catch (e) {
+    console.log(e);
+  }
 }
 render();
